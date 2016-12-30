2e8787738d50bf1f0127e9d7ddda9231b05f5338
plkreuzer <robothefan@gmail.com>
2016-12-30 17:50:34 -0600

    Initial commit of baseline files.
    This is the project at its initial stage. Seems to work OK for now.

---
+++ README.md
@@ -0,0 +1,5 @@
+A simple motion capture Python program to use the Raspberrypi B+ v3 and the No-IR v2 camera. 
+
+The motion capture piece is based on Adrian Rosebrock's algorithm at:
+
+http://www.pyimagesearch.com/2015/06/01/home-surveillance-and-motion-detection-with-the-raspberry-pi-python-and-opencv/
---
+++ pyimagesearch/tempimage.py
@@ -0,0 +1,14 @@
+# import the necessary packages
+import uuid
+import os
+ 
+class TempImage:
+    def __init__(self, basePath="/tmp/", ext=".jpg"):
+        # construct the file path
+        self.path = "{base_path}/{rand}{ext}".format(base_path=basePath,
+            rand=str(uuid.uuid4()), ext=ext)
+ 
+    def cleanup(self):
+        # remove the file
+        if os.access(self.path, os.W_OK):
+            os.remove(self.path)
